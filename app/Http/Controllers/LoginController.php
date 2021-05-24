258dfd3f453c6231571e7e6d7017ccda53fb4f88
DESKTOP-JNBEER3\User <hossien273gapeleh@gmail.com>
2021-05-24 08:50:49 -0700

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 
 class LoginController extends Controller
 {
     public function Login(Request $request)
     {
+        $data = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
 
+
+        if(!auth()->attempt($data)){
+            return response(['data' => ['success' => false,'massage' => 'ایمیل یا پسورد شما اشتباه است.']],403);
+        }
+
+        $user             = Auth::user();
+
+        $token = $user->createToken($user->email.'-'.now(), [$user->role]);
+        return response([ 'data' => ['success' => true , 'body' => $token ]] , Response::HTTP_OK);
     }
 }
